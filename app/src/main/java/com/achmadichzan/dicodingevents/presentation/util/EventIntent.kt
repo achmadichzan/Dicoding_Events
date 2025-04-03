782ec9e46dbcd270eaad297d67c1bd79ac293b1c
achmadichzan <122871669+achmadichzan@users.noreply.github.com>
2025-04-03 08:59:56 +0800

    Refactor: Change EventState and EventIntent to data objects
    The `Loading` state in `EventState` and `LoadAllEvents` intent in `EventIntent` are modified to be data objects. This change allows them to have potential future extensions.

--- app/src/main/java/com/achmadichzan/dicodingevents/presentation/util/EventIntent.kt
+++ app/src/main/java/com/achmadichzan/dicodingevents/presentation/util/EventIntent.kt
@@ -1,6 +1,6 @@
 package com.achmadichzan.dicodingevents.presentation.util
 
 sealed class EventIntent {
-    object LoadAllEvents : EventIntent()
+    data object LoadAllEvents : EventIntent()
     data class LoadEventDetail(val eventId: Int) : EventIntent()
 }
--- app/src/main/java/com/achmadichzan/dicodingevents/presentation/util/EventState.kt
+++ app/src/main/java/com/achmadichzan/dicodingevents/presentation/util/EventState.kt
@@ -3,7 +3,7 @@ package com.achmadichzan.dicodingevents.presentation.util
 import com.achmadichzan.dicodingevents.domain.model.ListEventsItem
 
 sealed class EventState {
-    object Loading : EventState()
+    data object Loading : EventState()
     data class Success(val events: List<ListEventsItem>) : EventState()
     data class SuccessDetail(val event: ListEventsItem) : EventState()
     data class Error(val message: String) : EventState()
