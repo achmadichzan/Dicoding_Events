b3d3084ce4b4edc2a9c7cc1f1abecaccc14c54d7
achmadichzan <122871669+achmadichzan@users.noreply.github.com>
2025-04-03 09:40:28 +0800

    feat: Replace Ktor client engine from CIO to Android
    The Ktor HTTP client engine was changed from `CIO` to `Android`. This change also includes adding the `ktor-client-android` dependency in `libs.versions.toml`.

--- app/src/main/java/com/achmadichzan/dicodingevents/data/di/AppModule.kt
+++ app/src/main/java/com/achmadichzan/dicodingevents/data/di/AppModule.kt
@@ -8,7 +8,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.Logging
@@ -25,7 +25,7 @@ object AppModule {
     @Provides
     @Singleton
     fun provideHttpClient(): HttpClient {
-        return HttpClient(CIO) {
+        return HttpClient(Android) {
             defaultRequest {
                 header(HttpHeaders.ContentType, ContentType.Application.Json)
             }
