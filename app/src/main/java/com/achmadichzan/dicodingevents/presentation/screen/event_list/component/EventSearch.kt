9fa63d508503dc9f903c280dc17a43b9a12cc377
achmadichzan <122871669+achmadichzan@users.noreply.github.com>
2025-04-03 23:39:13 +0800

    feat: Add IME padding to event search bar
    The event search bar now includes `imePadding` to adjust its layout when the on-screen keyboard is visible.

--- app/src/main/java/com/achmadichzan/dicodingevents/presentation/screen/event_list/component/EventSearch.kt
+++ app/src/main/java/com/achmadichzan/dicodingevents/presentation/screen/event_list/component/EventSearch.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -52,6 +53,7 @@ fun EventSearch(
                     .weight(1f)
                     .padding(vertical = 5.dp)
                     .height(50.dp)
+                    .imePadding()
             )
         }
     }
